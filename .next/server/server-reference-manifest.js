2156fc6c186b908c0d04b3508f6248aa018e9420
Thanuga-Rodrigo <144921624+Thanuga-Rodrigo@users.noreply.github.com>
2025-10-26 12:27:46 +0530

    remove build

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"ev4lFHpabPu4PZUqpTLVcP5aFyiitRaDyUDYLU8usBI=\"\n}"
--- .next/static/chunks/monkey-banana_src_app_page_tsx_8fc7a1f1._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/monkey-banana_src_5dbcad75._.js",
-  "static/chunks/ab5c8_7b743a6a._.js"
-],
-    source: "dynamic"
-});
